abf8a3a5c0a22c268873e982f2d0091b9cd059a4
Avery Schreiner <schreineravery@gmail.com>
2025-01-04 10:36:19 -0600

    nickname to required. renamed profile url

--- Models/Tables/User.cs
+++ Models/Tables/User.cs
@@ -7,8 +7,8 @@ public class User
 {
     [DynamoDBHashKey]
     public required string Id { get; set; }
-    public string? Nickname { get; set; }
-    public string? ProfilePictureUrl { get; set; }
+    public required string Nickname { get; set; }
+    public string? PictureUrl { get; set; }
     public string? Bio { get; set ; }
     public Track? Anthem { get; set; }
     public required HashSet<string> Followers { get; set; }
