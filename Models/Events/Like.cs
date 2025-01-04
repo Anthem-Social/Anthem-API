04af24b83234bf02b3819c6a2a50a073a612a399
Avery Schreiner <schreineravery@gmail.com>
2025-01-04 10:35:17 -0600

    init like table

--- Models/Events/Like.cs
+++
@@ -1,8 +0,0 @@
-namespace AnthemAPI.Models;
-
-public class Like
-{
-    public required User User { get; set; }
-    public required string PostId { get; set; }
-    public required DateTime Timestamp { get; set; }
-}
---
+++ Models/Tables/Like.cs
@@ -0,0 +1,17 @@
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AnthemAPI.Models;
+
+[DynamoDBTable("Likes")]
+public class Like
+{
+    [DynamoDBHashKey]
+    public required string PostId { get; set; }
+    [DynamoDBRangeKey]
+    public required string UserId { get; set; }
+    public required DateTime Timestamp { get; set; }
+    [DynamoDBGlobalSecondaryIndexHashKey]
+    public string UserIdIndex => UserId;
+    [DynamoDBGlobalSecondaryIndexRangeKey]
+    public DateTime TimestampIndex => Timestamp;
+}
