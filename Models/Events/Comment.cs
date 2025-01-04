b41e9167b4aae0f0b80824e107abb8163c827d03
Avery Schreiner <schreineravery@gmail.com>
2025-01-04 10:35:26 -0600

    init comments table

--- Models/Events/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace AnthemAPI.Models;
-
-public class Comment
-{
-    public required User User { get; set; }
-    public required string PostId { get; set; }
-    public required DateTime Timestamp { get; set; }
-    public required string Text { get; set; }
-}
---
+++ Models/Tables/Comment.cs
@@ -0,0 +1,19 @@
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AnthemAPI.Models;
+
+[DynamoDBTable("Comments")]
+public class Comment
+{
+    [DynamoDBHashKey]
+    public required string PostId { get; set; }
+    [DynamoDBRangeKey]
+    public required string CommentId { get; set; }
+    public required string UserId { get; set; }
+    public required string Text { get; set; }
+    public required DateTime Timestamp { get; set; }
+    [DynamoDBGlobalSecondaryIndexHashKey]
+    public string UserIdIndex => UserId;
+    [DynamoDBGlobalSecondaryIndexRangeKey]
+    public DateTime TimestampIndex => Timestamp;
+}
