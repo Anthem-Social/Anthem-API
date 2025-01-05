6067964c69f3d06dcef7e5e98f76f566356e6af8
Avery Schreiner <schreineravery@gmail.com>
2025-01-04 20:39:31 -0600

    add premium bool

--- Models/Tables/User.cs
+++ Models/Tables/User.cs
@@ -7,6 +7,7 @@ public class User
 {
     [DynamoDBHashKey]
     public required string Id { get; set; }
+    public required bool Premium { get; set; }
     public required string Nickname { get; set; }
     public string? PictureUrl { get; set; }
     public string? Bio { get; set ; }
