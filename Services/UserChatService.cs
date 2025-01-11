e67d32c299792a9c6dd26ef927ad1d280197f4e3
Avery Schreiner <schreineravery@gmail.com>
2025-01-11 13:50:26 -0600

    add delete and save

--- Services/UserChatService.cs
+++ Services/UserChatService.cs
@@ -4,6 +4,7 @@ using Amazon.DynamoDBv2.DocumentModel;
 using AnthemAPI.Common;
 using AnthemAPI.Common.Helpers;
 using AnthemAPI.Models;
+using Microsoft.AspNetCore.Components.Web;
 using static AnthemAPI.Common.Constants;
 
 namespace AnthemAPI.Services;
@@ -16,37 +17,83 @@ public class UserChatService
     {
         _context = new DynamoDBContext(db);
     }
+    
+    public async Task<ServiceResult<UserChat>> Save(UserChat userChat)
+    {
+        try
+        {
+            await _context.SaveAsync(userChat);
+            return ServiceResult<UserChat>.Success(userChat);
+        }
+        catch (Exception e)
+        {
+            return ServiceResult<UserChat>.Failure(e, $"Failed to save.", "UserChatService.Save()");
+        }
+    }
+
+    public async Task<ServiceResult<bool>> Delete(string userId, string chatId)
+    {
+        try
+        {
+            var query = new QueryOperationConfig
+            {
+                KeyExpression = new Expression
+                {
+                    ExpressionStatement = "UserId = :userId",
+                    ExpressionAttributeValues = new Dictionary<string, DynamoDBEntry>
+                    {
+                        { ":userId", userId }
+                    }
+                },
+                Filter = new QueryFilter("ChatId", QueryOperator.Equal, chatId)
+            };
+
+            var search = _context.FromQueryAsync<UserChat>(query);
+
+            List<UserChat> userChats = await search.GetRemainingAsync();
+
+            var deletions = userChats.Select(async x => await _context.DeleteAsync(x));
+
+            await Task.WhenAll(deletions);
+
+            return ServiceResult<bool>.Success(true);
+        }
+        catch (Exception e)
+        {
+            return ServiceResult<bool>.Failure(e, $"Failed to delete {chatId} for {userId}.", "UserChatService.Delete()");
+        }
+    }
 
     public async Task<ServiceResult<List<UserChat>>> GetBatch(string userId, int page)
     {
-    try
-    {
-        var config = new QueryOperationConfig
+        try
         {
-            KeyExpression = new Expression
+            var config = new QueryOperationConfig
             {
-                ExpressionStatement = "UserId = :userId",
-                ExpressionAttributeValues = new Dictionary<string, DynamoDBEntry>
+                KeyExpression = new Expression
                 {
-                    { ":userId", userId }
-                }
-            },
-            BackwardSearch = true,
-            Limit = DYNAMO_DB_BATCH_SIZE,
-            PaginationToken = page > 1 ? Helpers.CalculatePaginationToken(page) : null
-        };
+                    ExpressionStatement = "UserId = :userId",
+                    ExpressionAttributeValues = new Dictionary<string, DynamoDBEntry>
+                    {
+                        { ":userId", userId }
+                    }
+                },
+                BackwardSearch = true,
+                Limit = DYNAMO_DB_BATCH_SIZE,
+                PaginationToken = page > 1 ? Helpers.CalculatePaginationToken(page) : null
+            };
 
-        var search = _context.FromQueryAsync<UserChat>(config);
-        var userChats = new List<UserChat>();
-        
-        var results = await search.GetRemainingAsync();
-        userChats.AddRange(results);
+            var search = _context.FromQueryAsync<UserChat>(config);
+            var userChats = new List<UserChat>();
+            
+            var results = await search.GetRemainingAsync();
+            userChats.AddRange(results);
 
-        return ServiceResult<List<UserChat>>.Success(userChats);
-    }
-    catch (Exception e)
-    {
-        return ServiceResult<List<UserChat>>.Failure(e, $"Failed to get batch.", "UserChatService.GetBatch()");
-    }
+            return ServiceResult<List<UserChat>>.Success(userChats);
+        }
+        catch (Exception e)
+        {
+            return ServiceResult<List<UserChat>>.Failure(e, $"Failed to get batch.", "UserChatService.GetBatch()");
+        }
     }
 }
