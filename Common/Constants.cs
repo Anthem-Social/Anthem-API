dda39adf5fb58ab9e9bf90fc1fd699c80a9f1ef1
Avery Schreiner <schreineravery@gmail.com>
2025-01-11 17:08:42 -0600

    add user chat and message limits

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -4,7 +4,9 @@ public record PollingTier(int IntervalSeconds, string Group);
 
 public static class Constants
 {
-    public const int DYNAMO_DB_BATCH_SIZE = 25;
+    public const int DYNAMO_DB_BATCH_LIMIT = 25;
+    public const int MESSAGE_BATCH_LIMIT = 40;
+    public const int USER_CHAT_BATCH_LIMIT = 15;
     public static readonly PollingTier Active = new PollingTier(15, "Active");
     public static readonly PollingTier Reduced = new PollingTier(120, "Reduced");
 }
