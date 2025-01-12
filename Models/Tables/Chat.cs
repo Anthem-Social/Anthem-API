2f71b8fca0a2e4c36df436bad0ec08ff30934314
Avery Schreiner <schreineravery@gmail.com>
2025-01-12 13:13:44 -0600

    reorder properties

--- Models/Tables/Chat.cs
+++ Models/Tables/Chat.cs
@@ -9,8 +9,8 @@ public class Chat
     public required string Id { get; set; }
     public required string Name { get; set; }
     public required HashSet<string> UserIds { get; set; }
-    public required string CreatorUserId { get; set; }
-    public required DateTime CreatedAt { get; set; }
     public required DateTime LastMessageAt { get; set; }
     public required string Preview { get; set; }
+    public required string CreatorUserId { get; set; }
+    public required DateTime CreatedAt { get; set; }
 }
