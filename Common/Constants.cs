97815cddfc6f309b4e236b40e2f23b784cf23f23
Avery Schreiner <schreineravery@gmail.com>
2025-01-12 23:47:48 -0600

    set chat batch, batch get item, and message batch limits

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -4,9 +4,10 @@ public record PollingTier(int IntervalSeconds, string Group);
 
 public static class Constants
 {
-    public const int DYNAMO_DB_BATCH_LIMIT = 25;
+    public const int CHAT_BATCH_LIMIT = 20;
+    public const int DYNAMO_DB_BATCH_GET_ITEM_LIMIT = 100;
+    public const int DYNAMO_DB_BATCH_EXECUTE_STATEMENT_LIMIT = 25;
     public const int MESSAGE_BATCH_LIMIT = 40;
-    public const int USER_CHAT_BATCH_LIMIT = 15;
     public static readonly PollingTier Active = new PollingTier(15, "Active");
     public static readonly PollingTier Reduced = new PollingTier(120, "Reduced");
 }
