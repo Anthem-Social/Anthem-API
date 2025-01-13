a73303481d88688db203882d3b69538c1c82866e
Avery Schreiner <schreineravery@gmail.com>
2025-01-12 23:48:52 -0600

    implement id by hand

--- Models/Tables/Message.cs
+++ Models/Tables/Message.cs
@@ -9,9 +9,9 @@ public class Message
     [DynamoDBHashKey]
     public required string ChatId { get; set; }
     [DynamoDBRangeKey]
-    public string Id => $"{SentAt}#{UserId}";
+    public required string Id { get; set; } // Should be => $"{CreatedAt}#{UserId}"
     public required string UserId { get; set; }
-    public required DateTime SentAt { get; set; }
+    public required DateTime CreatedAt { get; set; }
     public required ContentType ContentType { get; set; }
     public required string Content { get; set; }
 }
