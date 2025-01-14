56e24f8a25419ed5865aaf682d6a154e8a51ebb0
Avery Schreiner <schreineravery@gmail.com>
2025-01-13 22:53:15 -0600

    implement add chat id

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,5 +1,6 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
 using AnthemAPI.Common;
 using AnthemAPI.Models;
 
@@ -7,10 +8,13 @@ namespace AnthemAPI.Services;
 
 public class UserService
 {
+    private readonly IAmazonDynamoDB _client;
     private readonly DynamoDBContext _context;
+    private const string TABLE_NAME = "Users";
 
     public UserService(IAmazonDynamoDB client)
     {
+        _client = client;
         _context = new DynamoDBContext(client);
     }
 
@@ -37,6 +41,35 @@ public class UserService
         catch (Exception e)
         {
             return ServiceResult<User>.Failure(e, $"Failed to save for {user.Id}.", "UserService.Save()");
+        }
+    }
+
+    public async Task<ServiceResult<User?>> AddChatId(List<string> ids, string chatId)
+    {
+        try
+        {
+            var batch = new BatchExecuteStatementRequest
+            {
+                Statements = ids.Select(userId => new BatchStatementRequest
+                {
+                    Statement = $"UPDATE {TABLE_NAME}" +
+                                " SET ChatIds = SET_ADD(ChatIds, ?)" +
+                                " WHERE Id = ?",
+                    Parameters = new List<AttributeValue>
+                    {
+                        new AttributeValue { SS = [chatId] },
+                        new AttributeValue { S = userId }
+                    }
+                }).ToList()
+            };
+
+            await _client.BatchExecuteStatementAsync(batch);
+
+            return ServiceResult<User?>.Success(null);
+        }
+        catch (Exception e)
+        {
+            return ServiceResult<User?>.Failure(e, $"Failed to add chat id.", "UserService.AddChatId()");
         }
     }
 }
