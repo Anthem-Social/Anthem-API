1b64e2f9981db22d3d8dfe62e065d8bba079b603
Avery Schreiner <schreineravery@gmail.com>
2025-01-13 22:52:49 -0600

    get users chats

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using AnthemAPI.Models;
 using AnthemAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AnthemAPI.Controllers;
 
@@ -8,28 +9,51 @@ namespace AnthemAPI.Controllers;
 [Route("users")]
 public class UsersController
 (
+    ChatService chatService,
     UserService userService
 ): ControllerBase
 {
+    private readonly ChatService _chatService = chatService;
     private readonly UserService _userService = userService;
 
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(string id)
     {
-        var user = await _userService.Load(id);
+        var load = await _userService.Load(id);
 
-        if (user.IsFailure)
+        if (load.IsFailure)
             return StatusCode(500);
 
-        if (user.Data is null)
+        if (load.Data is null)
             return NotFound();
 
-        return Ok(user.Data);
+        return Ok(load.Data);
     }
 
     [HttpPost("{id}/follower/{followerId}")]
     public async Task<IActionResult> Follow(string id, string followerId)
     {
         return Ok();
+    }
+
+    [HttpGet("{id}/chats")]
+    public async Task<IActionResult> GetChats(string id, [FromQuery] int page = 1)
+    {
+        var loadUser = await _userService.Load(id);
+
+        if (loadUser.IsFailure)
+            return StatusCode(500);
+
+        if (loadUser.Data is null)
+            return NotFound();
+        
+        List<string> chatIds = loadUser.Data.ChatIds.ToList();
+
+        var getChats = await _chatService.GetPage(chatIds, page);
+
+        if (getChats.IsFailure)
+            return StatusCode(500);
+
+        return Ok(getChats.Data);
     }
 }
