4fb0cf5f6c729d1715daeb25f556fd7f72356f0b
Avery Schreiner <schreineravery@gmail.com>
2025-01-19 22:35:19 -0600

    init

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("posts")]
+public class PostsController: ControllerBase
+{
+    // [HttpPost]
+    // [HttpGet("{postId}")]
+    // [HttpDelete("{postId}")]
+    // [HttpPost("{postId}/likes")]
+    // [HttpGet("{postId}/likes")]
+    // [HttpDelete("{postId}/likes/{likeId}")]
+    // [HttpPost("{postId}/comments")]
+    // [HttpGet("{postId}/comments")]
+    // [HttpDelete("{postId90-8-}/comments/{commentId}")]
+}
