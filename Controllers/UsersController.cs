c55b64ee3a4db791264701b9c599e3ab8cda902f
Avery Schreiner <schreineravery@gmail.com>
2025-01-20 11:34:55 -0600

    implement create follow, delete follow, get followers, and get following

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -9,17 +9,13 @@ namespace AnthemAPI.Controllers;
 public class UsersController
 (
     ChatService chatService,
-    FollowService followService,
-    StatusConnectionService statusConnectionService,
-    StatusJobService statusJobService,
+    FollowerService followerService,
     StatusService statusService,
     UserService userService
 ): ControllerBase
 {
     private readonly ChatService _chatService = chatService;
-    private readonly FollowService _followService = followService;
-    private readonly StatusConnectionService _statusConnectionService = statusConnectionService;
-    private readonly StatusJobService _statusJobService = statusJobService;
+    private readonly FollowerService _followerService = followerService;
     private readonly StatusService _statusService = statusService;
     private readonly UserService _userService = userService;
 
@@ -37,22 +33,61 @@ public class UsersController
         return Ok(load.Data);
     }
 
-    [HttpPost("{userId}/follower/{followerUserId}")]
-    public async Task<IActionResult> Follow(string userId, string followerUserId)
+    [HttpPost("{userId}/followers/{followerUserId}")]
+    public async Task<IActionResult> CreateFollower(string userId, string followerUserId)
     {
-        var follow = new Follow
+        var follower = new Follower
         {
-            Followee = userId,
-            Follower = followerUserId,
+            UserId = userId,
+            FollowerUserId = followerUserId,
             CreatedAt = DateTime.UtcNow
         };
 
-        var save = await _followService.Save(follow);
+        var save = await _followerService.Save(follower);
 
         if (save.IsFailure)
             return StatusCode(500);
         
         return Created();
+    }
+
+    [HttpDelete("{userId}/followers/{followerUserId}")]
+    public async Task<IActionResult> DeleteFollower(string userId, string followerUserId)
+    {
+        var delete = await _followerService.Delete(userId, followerUserId);
+
+        if (delete.IsFailure)
+            return StatusCode(500);
+        
+        return NoContent();
+    }
+
+    [HttpGet("{userId}/followers")]
+    public async Task<IActionResult> GetFollowers(string userId, [FromQuery] int page = 1)
+    {
+        var load = await _followerService.LoadFollowers(userId, page);
+
+        if (load.IsFailure)
+            return StatusCode(500);
+        
+        if (load.Data is null)
+            return NotFound();
+
+        return Ok(load.Data);
+    }
+
+    [HttpGet("{userId}/following")]
+    public async Task<IActionResult> GetFollowing(string userId, [FromQuery] int page = 1)
+    {
+        var load = await _followerService.LoadFollowing(userId, page);
+
+        if (load.IsFailure)
+            return StatusCode(500);
+        
+        if (load.Data is null)
+            return NotFound();
+
+        return Ok(load.Data);
     }
 
     [HttpGet("{userId}/chats")]
