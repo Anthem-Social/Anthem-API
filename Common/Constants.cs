e4855d9ea211a084ffb946e57da205553e84bb02
Avery Schreiner <schreineravery@gmail.com>
2025-01-22 21:51:10 -0600

    remove chat batch limit

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -4,8 +4,7 @@ public record PollingTier(int IntervalSeconds, string Group);
 
 public static class Constants
 {
-    public const int CHAT_BATCH_LIMIT = 20;
-    public const int DYNAMO_DB_BATCH_GET_ITEM_LIMIT = 100;
+    public const int DYNAMO_DB_BATCH_GET_LIMIT = 100;
     public const int DYNAMO_DB_BATCH_EXECUTE_STATEMENT_LIMIT = 25;
     public const int FOLLOW_BATCH_LIMIT = 50;
     public static readonly PollingTier Active = new PollingTier(15, "Active");
