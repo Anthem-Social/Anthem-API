b114ba64850781af7d4dcd4881d697826388af0b
Avery Schreiner <schreineravery@gmail.com>
2025-03-30 23:46:07 -0500

    load all statuses given a list of user ids

--- Services/StatusesService.cs
+++ Services/StatusesService.cs
@@ -2,6 +2,7 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using AnthemAPI.Common;
 using AnthemAPI.Models;
+using static AnthemAPI.Common.Constants;
 
 namespace AnthemAPI.Services;
 
@@ -24,6 +25,34 @@ public class StatusesService
         catch (Exception e)
         {
             return ServiceResult<Status?>.Failure(e, $"Failed to load for {userId}.", "StatusesService.Load()");
+        }
+    }
+
+    public async Task<ServiceResult<List<Status>>> LoadAll(List<string> userIds)
+    {
+        try
+        {
+            var batches = new List<BatchGet<Status>>();
+
+            for (int i = 0; i < userIds.Count; i += DYNAMO_DB_BATCH_GET_LIMIT)
+            {
+                List<string> ids  = userIds.Skip(i).Take(DYNAMO_DB_BATCH_GET_LIMIT).ToList();
+                var batch = _context.CreateBatchGet<Status>();
+                ids.ForEach(batch.AddKey);
+                batches.Add(batch);
+            }
+
+            await _context.ExecuteBatchGetAsync(batches.ToArray());
+
+            List<Status> statuses = batches
+                .SelectMany(batch => batch.Results)
+                .ToList();
+            
+            return ServiceResult<List<Status>>.Success(statuses);
+        }
+        catch (Exception e)
+        {
+            return ServiceResult<List<Status>>.Failure(e, "Failed to load all.", "StatusesService.LoadAll()");
         }
     }
 
